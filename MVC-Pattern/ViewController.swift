3bb992be18f4640b49b2dbb06712dd6258a899d1
渡邊丈洋 <takehiro@watanabejouyounomacbook-air.local>
2019-03-10 17:43:23 +0900

    Initial Commit

---
+++ MVC-Pattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVC-Pattern
+//
+//  Created by 渡邊丈洋 on 2019/03/10.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
