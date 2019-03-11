171a98d78aa2607fe8a5cf03ae68da8028d1aba6
tkhrname <takehiro.watanabe.224@gmail.com>
2019-03-11 23:50:44 +0900

    Add TaskListCell

---
+++ MVC-Pattern/View/TaskListCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskListCell.swift
+//  MVC-Pattern
+//
+//  Created by 渡邊丈洋 on 2019/03/11.
+//  Copyright © 2019 渡邊丈洋. All rights reserved.
+//
+
+import UIKit
+
+class TaskListCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
